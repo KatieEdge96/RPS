5ce8271106d336e61a140ee5621f1506939bad62
Katie Edge <K.L.Edge@hotmail.com>
2022-07-04 19:12:51 +0100

    JS playGame & js-script added into html

--- RPS.js
+++ RPS.js
@@ -2,7 +2,16 @@
 
 let computerScore = 0;
 let playerScore = 0;
-let roundWinner = ''
+let moves = 0;
+
+const playGame = () => {
+    const rockBtn = document.querySelector('.rock');
+    const paperBtn = document.querySelector('.paper');
+    const scissorBtn = document.querySelector('.scissor');
+    const playerOptions = [rockBtn,paperBtn,scissorBtn];
+    const computerOptions = ['rock','paper','scissor'];
+    
+}
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
