2515c9fb43888868857ab0080db6f5908395579c
Katie Edge <K.L.Edge@hotmail.com>
2022-07-04 19:51:30 +0100

    javascript - start - need to rework

--- RPS.js
+++ RPS.js
@@ -1,17 +1,43 @@
 
 
-let computerScore = 0;
-let playerScore = 0;
-let moves = 0;
+let computerScore = 0 ;
+let playerScore = 0 ;
+let moves = 0 ;
 
 const playGame = () => {
-    const rockBtn = document.querySelector('.rock');
-    const paperBtn = document.querySelector('.paper');
-    const scissorBtn = document.querySelector('.scissor');
-    const playerOptions = [rockBtn,paperBtn,scissorBtn];
-    const computerOptions = ['rock','paper','scissor'];
-    
+
+    const rockBtn = document.querySelector('.rock') ;
+    const paperBtn = document.querySelector('.paper') ;
+    const scissorBtn = document.querySelector('.scissor') ;
+    const playerOptions = [rockBtn,paperBtn,scissorBtn] ;
+    const computerOptions = ['rock','paper','scissor'] 
+
+
+playerOptions.forEach(option => {
+    option.addEventListener('click'.function() { 
+
+        const movesLeft = document.querySelector('.movesLeft') ,
+        moves++;
+        movesLeft.innerText = 'Moves Remaining: ${5-moves}';
+
+        const choiceNumber = Math.floor(Math.random()*3) ;
+        const computerChoice = computerOptions[choiceNumber] ;
+
+        winner(this.innerText.computerChoice)
+
+        if(moves == 5){
+            gameOver(playerOptions.movesLeft) ;
+        }
+
+    })
+})
+
 }
+        
+       
+
+    
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
